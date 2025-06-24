d90e7ee39dcf64e977b69c09a4aad12129d7b03f
Geniuska275 <aigbojie2020@gmail.com>
2025-06-24 09:23:57 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,61 @@
-import React, { useEffect } from 'react';
-import WebFont from 'webfontloader';
 
 function App() {
   
   
-  useEffect(() => {
-      WebFont.load({
-        google: {
-          families: ['Clutch'] // Or the specific font name
-        }
-      });
-    }, []);
+  
 
   return (
     <div className="heading">
-      <h4 style={{
-        wordSpacing:"10px",
-        letterSpacing:"5px",
-        marginLeft:"60px"
-      }}>Paul Elisha</h4>
+      <p style={{
+        marginLeft:"20px",
+        fontWeight:"bold"
+      }}>Paul Elisha</p>
       <p style={{
       
-        marginTop: '20px',
-        wordSpacing:"10px",
-        letterSpacing:"5px",
-        marginLeft:"60px"
+        marginTop: '10px',
+        marginLeft:"20px",
+        fontWeight:"bold"
 
-      }}>Product,Engineer,Thinker.</p>
+      }}>Engineer and Researcher</p>
       <div style={{
-        marginTop:"40px",
-         letterSpacing:"5px",
-        marginLeft:"60px"
+        marginTop:"10px",
+         
+        marginLeft:"10px"
       }}>
-      <p>Stints:</p>
+      <p style={{
+         marginLeft:"10px"
+      }}>Stints:</p>
       <ul style={{
-        padding:"20px"
+        // padding:"2px"
       }}>
-        <li>ml & product <a href=''>
+        <li>Dev ambassador, <a href=''>
 
-           @linkedin
+           @kaiafoundation;
           </a>
-           ; flagship, building ml models to understand human potential & match that at scale.</li>
-        <li>core ml
+           building backend sdk, smart contract development ,engaging the developer community and solving problems.</li>
+        <li>Open source contribution,
           <a href=''>
-          @apple; 
-
+          @bifrost; 
+            </a>  <a href=''>
+          @uniswap; 
             </a> 
-          wireless technologies, model pruning/quantization, inference apis + data pipeline (apple home ecosystem)</li>
-         <li>
-          research @nasa: payload rocket RF communications + telecommand control signals transmission (eyes in the cloud, usli)
-         </li>
-         <li>
-          data-center scale ml compute infra @usc/isi
-         </li>
-         <li>
-          oil & gas energy data layer @geospectra engineering
-         </li>
-         <li>
-          Selected Research
+         improved technical documentation on Uniswap V4,deployed a hook to maximize yield on bifrost.</li>
+         
+         <li style={{
+          fontWeight:"bold"
+         }}>
+          Selected Publications
          </li>
          <ul>
-          <li>Multidimensional lattice for Post-Quantum Cryptographic Key Generation with Dr. Paul Wang</li>
-          <li> temporospatial ground plane morphology analysis + semantic segmentation & trajectory mapping, faculty - Dr. Kofi Nyarko
+          <li> <a href='https://paragraph.com/@break-into-defi/uniswap-v3-advanced-mechanics'>Uniswap V3 Advanced Mechanic:</a> <br></br>
+          a break down involving math and Mechanics of the underlying protocol.
           </li>
-          <li>disease modeling, stochastic processes, and differential equations research - scook@tarleton.edu</li>
+           <li> <a href='https://paragraph.com/@break-into-defi/uniswap-v2-advanced-mechanics'>Uniswap V2 Advanced Mechanic:</a> <br></br>
+            a break down involving liquidity math and Mechanics.
+          </li>
          </ul>
       </ul>
-      <p>building products, designing, scaling and tennis.</p>
+      {/* <p>building products, designing, scaling and tennis.</p> */}
 
 
       </div>
@@ -74,16 +63,17 @@ function App() {
         marginTop: '20px',
         wordSpacing:"10px",
         letterSpacing:"5px",
-        marginLeft:"60px"
-      }}><a href=''>@github</a> <a href=''>@linkedin</a> <a href=''>Twitter</a></p>
+        marginLeft:"20px"
+      }}><a href='https://github.com/pualelisha'>@github</a> <a href='https://www.linkedin.com/in/paul-elisha-659268198/'>@linkedin</a> <a href='https://x.com/paulelisha'>Twitter</a></p>
         <p
         style={{
         marginTop: '20px',
         wordSpacing:"10px",
         letterSpacing:"5px",
-        marginLeft:"60px"
+        marginLeft:"20px"
       }}
-        ><a href=''>PaulElisha@gmail.com</a></p>
+        ><a href='mailto:ajaoireoluwa1@gmail.com'>ajaoireoluwa1@gmail.com</a></p>
+        <p style={{textAlign:"center"}}>Paul Elisha @ 2025</p>
 
     </div>
   )
