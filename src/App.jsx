93a81d87df9374826a8c88d375982c53b7964974
Geniuska275 <aigbojie2020@gmail.com>
2025-06-24 14:14:04 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,77 +5,66 @@ function App() {
   
 
   return (
-    <div className="heading">
-      <p style={{
-        marginLeft:"20px",
-        fontWeight:"bold"
-      }}>Paul Elisha</p>
-      <p style={{
-      
-        marginTop: '10px',
-        marginLeft:"20px",
-        fontWeight:"bold"
-
-      }}>Engineer and Researcher</p>
-      <div style={{
-        marginTop:"10px",
-         
-        marginLeft:"10px"
-      }}>
-      <p style={{
-         marginLeft:"10px"
-      }}>Stints:</p>
-      <ul style={{
-        // padding:"2px"
-      }}>
-        <li>Dev ambassador, <a href=''>
-
-           @kaiafoundation;
-          </a>
-           building backend sdk, smart contract development ,engaging the developer community and solving problems.</li>
-        <li>Open source contribution,
-          <a href=''>
+     <body class="__variable_5cfdac __variable_9a8899 antialiased">
+        <div class="app">
+            <p>Paul Elisha</p>
+            <p>engineer, researcher and thinker</p>
+            <br/>
+            <p>Stints:</p>
+            <ul>
+                <li>
+                    dev ambassador, <a href="">@kaiafoundation</a> building backend sdk, smart contract development ,engaging the developer community and solving problems; 
+                </li>
+                <li>
+                    Open source contribution,
+         <a href='https://bifrost.io/'>
           @bifrost; 
-            </a>  <a href=''>
-          @uniswap; 
-            </a> 
-         improved technical documentation on Uniswap V4,deployed a hook to maximize yield on bifrost.</li>
-         
-         <li style={{
-          fontWeight:"bold"
-         }}>
-          Selected Publications
-         </li>
-         <ul>
-          <li> <a href='https://paragraph.com/@break-into-defi/uniswap-v3-advanced-mechanics'>Uniswap V3 Advanced Mechanic:</a> <br></br>
-          a break down involving math and Mechanics of the underlying protocol.
-          </li>
-           <li> <a href='https://paragraph.com/@break-into-defi/uniswap-v2-advanced-mechanics'>Uniswap V2 Advanced Mechanic:</a> <br></br>
-            a break down involving liquidity math and Mechanics.
-          </li>
-         </ul>
-      </ul>
-      {/* <p>building products, designing, scaling and tennis.</p> */}
+            </a>  <a href='https://docs.uniswap.org/'>
+        @uniswap; 
+            </a>    improved technical documentation on Uniswap V4,deployed a hook to maximize yield on bifrost.
+                </li>
+                {/* <li>
+                    research <a href="https://nasa.gov">@nasa</a>
+                    : payload rocket RF communications + telecommand control signals transmission (eyes in the cloud, usli)
+                </li> */}
+                {/* <li>
+                    data-center scale ml compute infra
+                 
+                    <a href="https://www.usc.edu/">@usc/isi</a>
+                </li> */}
+                {/* <li>
+                    oil &amp;gas energy data layer
+                  
+                    <a href="https://www.geospectraengineering.com/">@geospectra engineering</a>
+                </li> */}
+                <li>
+                    Selected Publications
+                    <ul>
+                        <li>
+                           <a href='https://paragraph.com/@break-into-defi/uniswap-v3-advanced-mechanics'>Uniswap V3 Advanced Mechanic:</a> a break down involving math and Mechanics of the underlying protocol.
+                        </li>
+                        <li>
+                           <a href='https://paragraph.com/@break-into-defi/uniswap-v2-advanced-mechanics'>Uniswap V2 Advanced Mechanic:</a> a break down involving liquidity math and Mechanics.
+                        </li>
+                        
+                    </ul>
+                </li>
+                <br/>
+            </ul>
+            <p>building sdks, helping developers, and a technical thinker. </p>
+            
+            <p class="socials">
+                <a href="https://github.com/pualelisha">github</a>
+                <a href="https://www.linkedin.com/in/paul-elisha-659268198/ ">linkedin</a>
+                <a href="https://x.com/paulelisha">twitter</a>
+            </p>
+            <p>
+                <a href="mailto:ajaoireoluwa1@gmail.com">ajaoireoluwa1@gmail.com</a>
+            </p>
+        </div>
+      
+    </body>
 
-
-      </div>
-      <p style={{
-        marginTop: '20px',
-        wordSpacing:"10px",
-        letterSpacing:"5px",
-        marginLeft:"20px"
-      }}><a href='https://github.com/pualelisha'>@github</a> <a href='https://www.linkedin.com/in/paul-elisha-659268198/'>@linkedin</a> <a href='https://x.com/paulelisha'>Twitter</a></p>
-        <p
-        style={{
-        marginTop: '20px',
-        wordSpacing:"10px",
-        letterSpacing:"5px",
-        marginLeft:"20px"
-      }}
-        ><a href='mailto:ajaoireoluwa1@gmail.com'>ajaoireoluwa1@gmail.com</a></p>
-        <p style={{textAlign:"center"}}>Paul Elisha @ 2025</p>
-
-    </div>
   )
 }
 
